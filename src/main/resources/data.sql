78753ffa77d0ab686425aa86b1d80b693278e840
cylin0201 <cylin0201@naver.com>
2026-05-09 04:45:50 +0900

    refactor: 더미데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,11 +22,11 @@ INSERT INTO ticket_events (title, category, keyword, venue_name, venue_address, 
 
 ('뮤지컬 오페라의 유령', 'MUSICAL', '오페라의유령 뮤지컬 브로드웨이', '블루스퀘어 신한카드홀', '서울특별시 용산구 이태원로 294', 37.5415021, 126.9936951,
  '세계적인 뮤지컬 오페라의 유령 내한공연. 감동의 무대를 만나보세요.',
- 'https://ap-northeast-2.console.aws.amazon.com/s3/object/khuthon-bucket?region=ap-northeast-2&prefix=events/7e7cb371-048f-430a-9d42-72e58d6ed211_%EC%98%A4%ED%8E%98%EB%9D%BC%EC%9D%98+%EC%9C%A0%EB%A0%B9.png', NOW(), NOW()),
+ 'https://khuthon-bucket.s3.ap-northeast-2.amazonaws.com/events/7e7cb371-048f-430a-9d42-72e58d6ed211_%EC%98%A4%ED%8E%98%EB%9D%BC%EC%9D%98+%EC%9C%A0%EB%A0%B9.png', NOW(), NOW()),
 
 ('T1 vs Gen.G 결승전', 'ESPORTS', 'T1 GenG 롤드컵 이스포츠 LoL', '인천 송도컨벤시아', '인천광역시 연수구 센트럴로 123', 37.3878012, 126.6432053,
  '2026 LoL Champions Korea 결승전. T1과 Gen.G의 최종 대결!',
- 'https://ap-northeast-2.console.aws.amazon.com/s3/object/khuthon-bucket?region=ap-northeast-2&prefix=events/7e7cb371-048f-430a-9d42-72e58d6ed211_%EC%98%A4%ED%8E%98%EB%9D%BC%EC%9D%98+%EC%9C%A0%EB%A0%B9.png', NOW(), NOW()),
+ 'https://khuthon-bucket.s3.ap-northeast-2.amazonaws.com/events/7e7cb371-048f-430a-9d42-72e58d6ed211_%EC%98%A4%ED%8E%98%EB%9D%BC%EC%9D%98+%EC%9C%A0%EB%A0%B9.png', NOW(), NOW()),
 ('연극 햄릿', 'MUSICAL', '햄릿 연극 명동예술극장 클래식', '명동예술극장', '서울특별시 중구 남대문로 2길 12', 37.5642, 126.9847,
  '셰익스피어의 4대 비극 중 하나인 햄릿. 명동예술극장에서 만나는 고전의 감동.',
  'https://khuthon-bucket.s3.ap-northeast-2.amazonaws.com/events/8c490851-d0f4-46df-b6c0-2efd90622786_%ED%96%84%EB%A6%BF.png', NOW(), NOW()),
